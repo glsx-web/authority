dfac7c4a0ca8cf5f08cce0231ac4c8288f7eb547
cwq0219 <2838673010@qq.com>
2018-08-30 20:13:50 +0800

    create

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,8 +1,15 @@
 <!-- roleManagement -->
 <template>
   <div>
-    <gl-input placeholder="请输入角色名称" v-model="roleName" clearable>
-    </gl-input>
+    <div class="default-style role-header">
+      <gl-input class="role-input" placeholder="请输入角色名称" v-model="roleName" clearable></gl-input>
+      <gl-button type="primary" @click="findRoleName">搜索</gl-button>
+      <gl-button class="create-button" size="small" @click="createRole">创建</gl-button>
+    </div>
+    <hr style="height:1px;border:0px;background-color:#e7eaec">
+    <div class="default-style">
+      <gl-table :table="roleData"></gl-table>
+    </div>
   </div>
 </template>
 
@@ -11,13 +18,68 @@ export default {
   name: 'role',
   data () {
     return {
-      roleName: ''
+      roleName: '',
+      roleData: {
+        data: [{
+          id: '1032',
+          name: '好车主',
+          department: '研发中心-网络软件部',
+          state: '开启',
+          createTime: '2018-08-12 13:00:00'
+        }],
+        column: [{
+          label: '序号',
+          prop: 'id'
+        }, {
+          label: '角色名字',
+          prop: 'name'
+        }, {
+          label: '所属部门',
+          prop: 'department'
+        }, {
+          label: '状态',
+          prop: 'state'
+        }, {
+          label: '创建时间',
+          prop: 'createTime'
+        }]
+      }
+    }
+  },
+  methods: {
+    findRoleName () {
+      this.roleName = ''
+    },
+    createRole () {
+      this.$alert('创建成功', '创建角色', {
+        confirmButtonTest: '确定',
+        callback: action => {
+          this.$message({
+            showClose: true,
+            type: 'success',
+            message: 'action: 创建成功'
+          })
+        }
+      })
     }
   }
 }
 </script>
 <style scoped>
-gl-input__innner{
-    width: 30%;
+.default-style {
+    padding: 0px 25px;
+}
+.role-header {
+    position: relative;
+    margin-bottom: 20px;
+}
+.role-input {
+    width: 40%;
+    margin-right: 5px;
+}
+.create-button {
+    position: absolute;
+    right: 25px;
+    bottom: 0px;
 }
 </style>
