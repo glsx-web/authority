edf43e0ad28d678b893af162e6b5893292362719
liwk <520200915@163.com>
2018-09-07 11:52:46 +0800

    添加菜单组件

---
+++ src/config/common.conf.js
@@ -0,0 +1,15 @@
+export default{
+  axios: {
+    baseURL: 'http://192.168.3.171:7300/mock/5b91805391b67469b43e2921/subsystem',
+    whiteList: ['vue/userList', 'vue/roleList'],
+    timeout: 15000,
+    result: {
+      data_key: 'data', // 返回值数据实体存放对象
+      message_key: 'message', // 返回信息字段名
+      code_key: 'returnCode', // 返回结果字段名
+      code_success_value: '0', // 返回成功值 注意类型 (字符串与数字)
+      code_message_map: { // 错误码 错误信息 映射表
+      }
+    }
+  }
+}
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,10 @@
 const titles = require('./title.js')
 const glob = require('glob')
 const pages = {}
+let path = require('path')
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
 // const mode = process.env.NODE_ENV || 'development'
 glob.sync('./src/views/**/app.js').forEach(path => {
   const chunk = path.split('./src/views/')[1].split('/app.js')[0]
@@ -14,7 +18,11 @@ glob.sync('./src/views/**/app.js').forEach(path => {
 })
 module.exports = {
   pages,
-  chainWebpack: config => config.plugins.delete('named-chunks'),
+  chainWebpack: config => {
+    config.plugins.delete('named-chunks')
+    config.resolve.alias
+      .set('$', resolve('src/views/components'))
+  },
   configureWebpack: (config) => {
     // config.devtool = (mode === 'development') ? 'inline-source-map' : false
     // config.mode = mode
